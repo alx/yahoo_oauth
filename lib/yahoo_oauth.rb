cd7ea58274424ea540397b9e88c89e976779532a
Alexandre Girard <alx.girard@gmail.com>
2011-03-02 21:39:04 +0100

    add delicious posts_add access

--- lib/yahoo_oauth.rb
+++ lib/yahoo_oauth.rb
@@ -4,6 +4,7 @@ require 'json'
 require 'yahoo_oauth/objects'
 require 'yahoo_oauth/client'
 require 'yahoo_oauth/social_directory'
+require 'yahoo_oauth/delicious'
 
 module YahooOAuth
 end
---
+++ lib/yahoo_oauth/delicious.rb
@@ -0,0 +1,29 @@
+#
+# A Ruby Library for Yahoo! Delicious API
+#
+# Author::    David Li (mailto:taweili@yahoo.com)
+# Copyright:: Copyright (c) 2010 David Li
+# License::   LGPL
+
+# Provides Delicious API
+# http://developer.yahoo.com/social/rest_api_guide/social_dir_api.html
+module YahooOAuth
+  class Client
+    
+    def posts_add(parameters)
+      query = parameters.map { |k, v| "#{escape(k.to_s)}=#{escape(v)}" } * '&'
+      components = ['http://api.del.icio.us/v2/posts/add']
+      components << query unless query.empty?
+      response = access_token.get(components * '?')
+      unless response.is_a?(Net::HTTPOK)
+        raise "HTTP response code: #{response.code}"
+      end
+      matches = Regexp.new('<result code="([^\"]*)" />').match(response.body)
+      unless matches && matches[1] == 'done'
+        raise "Delicious API code: '#{matches[1]}'"
+      end
+      response
+    end
+    
+  end
+end
